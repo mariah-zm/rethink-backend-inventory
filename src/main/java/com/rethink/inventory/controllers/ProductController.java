255eb2680353499df80e5644eb31e5c3dfdf3625
mariah.zm <mariah.zammit.18@um.edu.mt>
2022-01-17 23:53:39 +0100

    Implements product controller methods

--- src/main/java/com/rethink/inventory/controllers/ProductController.java
+++ src/main/java/com/rethink/inventory/controllers/ProductController.java
@@ -1,6 +1,11 @@
 package com.rethink.inventory.controllers;
 
+import com.rethink.inventory.exceptions.CategoryNotFoundException;
+import com.rethink.inventory.exceptions.ProductNotFoundException;
 import com.rethink.inventory.models.Product;
+import com.rethink.inventory.models.enums.CategoryName;
+import com.rethink.inventory.services.CategoryService;
+import com.rethink.inventory.services.ProductService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,16 +14,46 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    private final ProductService productService;
+    private final CategoryService categoryService;
+
+    public ProductController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/{productId}")
-    public @ResponseBody Product getProduct(@PathVariable int productId) {
-        // TODO Implement method
-        return null;
+    public @ResponseBody
+    Product getProduct(@PathVariable int productId) {
+        try {
+            return productService.getProduct(productId);
+        } catch (ProductNotFoundException e) {
+           return null;
+        }
+    }
+
+    @GetMapping("")
+    public @ResponseBody List<Product> getALlProducts() {
+        return productService.getAllProducts();
     }
 
     @GetMapping("/{name}")
-    public @ResponseBody List<Product> findProducts(@PathVariable String name) {
-        // TODO Implement method
-        return null;
+    public @ResponseBody
+    List<Product> findProductsByName(@PathVariable String name) {
+        try {
+            return productService.searchProductsByName(name);
+        } catch (ProductNotFoundException e) {
+            return null;
+        }
+    }
+
+    @GetMapping("/location")
+    public @ResponseBody Integer getProductLocation(@RequestBody CategoryName categoryName) {
+        try {
+            return categoryService.getCategoryLocation(categoryName);
+        } catch (CategoryNotFoundException e) {
+            return null;
+        }
     }
 
 }
