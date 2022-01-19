2f9027947fb61ea4663a860319933bd1492d4abb
mariah.zm <mariah.zammit.18@um.edu.mt>
2022-01-19 11:57:41 +0100

    Moves cors config to its own class

---
+++ src/main/java/com/rethink/inventory/config/WebClientConfig.java
@@ -0,0 +1,26 @@
+package com.rethink.inventory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**").
+                        allowedOrigins("*").
+                        allowedMethods("HEAD", "OPTIONS", "POST", "GET", "DELETE", "PATCH").
+                        exposedHeaders("Access-Control-Allow-Origin");
+            }
+        };
+    }
+
+}
