ff78be574a4b9a28557ec5b166a970bc6e2539f1
mariah.zm <mariah.zammit.18@um.edu.mt>
2022-01-19 12:24:53 +0100

    Another attempt at solving cors issue

--- src/main/java/com/rethink/inventory/config/WebClientConfig.java
+++ src/main/java/com/rethink/inventory/config/WebClientConfig.java
@@ -1,20 +1,36 @@
 package com.rethink.inventory.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-@EnableWebMvc
-public class WebClientConfig implements WebMvcConfigurer {
+public class WebClientConfig {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").
+//                allowedOrigins("*").
+//                allowedMethods("HEAD", "OPTIONS", "POST", "GET", "DELETE", "PATCH").
+//                exposedHeaders("Access-Control-Allow-Origin", "Content-Type", "Authorization", "Content-Length", "X-Requested-With");
+//    }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").
-                allowedOrigins("*").
-                allowedMethods("HEAD", "OPTIONS", "POST", "GET", "DELETE", "PATCH").
-                exposedHeaders("Access-Control-Allow-Origin", "Content-Type", "Authorization", "Content-Length", "X-Requested-With");
+    @Bean
+     public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
